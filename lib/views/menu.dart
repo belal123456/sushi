aa3226b9f5f18e7518b34c5fa7d76d61191ffa5e
belal_soliman <belalsoliman8@gmail.com>
2024-08-10 19:20:48 +0300

    Update menu images and paths in menu.dart
    Refactor image paths in the menu.dart file to use new image assets located in the assets/images directory. This includes updating the image paths for various sushi types (Nigiri, Maki, Sashimi, Temaki, Uramaki) to point to the new image files. Additionally, add new image files (3.png, sushi1.png) and remove obsolete file (sushi (2).png). Ensure the pubspec.yaml file is updated to include the new assets directory for proper asset resolution.

--- lib/views/menu.dart
+++ lib/views/menu.dart
@@ -16,30 +16,27 @@ class _MenuState extends State<Menu> {
   List foodModel = [
     FoodModel(
         name: "Nigiri",
-        imagePath:
-            "assets/menu-imgs/closeup-shot-delicious-sushi-roll-with-seasonings-white-background.jpg",
+        imagePath: "assets/images/sushi (3).png",
         price: "30",
         rating: "4.5"),
     FoodModel(
         name: "Maki",
-        imagePath:
-            "assets/menu-imgs/closeup-shot-sushi-roll-black-stone-plate.jpg",
+        imagePath: "assets/images/sushi (4).png",
         price: "50",
         rating: "4.4"),
     FoodModel(
         name: "Sashimi",
-        imagePath: "assets/menu-imgs/delicious-sushi-rolls.jpg",
+        imagePath: "assets/images/sushi (1).png",
         price: "30",
         rating: "4.7"),
     FoodModel(
         name: "Temaki",
-        imagePath:
-            "assets/menu-imgs/flat-lay-plate-sushi-rolls-with-nigiri.jpg",
+        imagePath: "assets/images/sushi.png",
         price: "93",
         rating: "4.8"),
     FoodModel(
         name: "Uramaki",
-        imagePath: "assets/menu-imgs/maki-sushi-isolated-white.jpg",
+        imagePath: "assets/images/sushi.png",
         price: "23",
         rating: "4.9"),
   ];
