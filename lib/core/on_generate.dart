b3e5602ba068506edb28cd9e534ed6c28abb2323
belal_soliman <belalsoliman8@gmail.com>
2024-08-10 16:15:52 +0300

    Add HomePage Route and Enhance Menu UI
    Introduce a new route for the HomePage in the onGenerateRoute function and
    enhance the UI of the Menu page with a promo banner, search bar, and
    styling improvements. This update includes the addition of a custom button
    widget import and extensive UI modifications to the Menu page, enhancing
    user experience and visual appeal.

--- lib/core/on_generate.dart
+++ lib/core/on_generate.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sushi/views/home_page.dart';
 
 import 'package:sushi/views/menu.dart';
 
@@ -6,6 +7,8 @@ Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case Menu.routeName:
       return MaterialPageRoute(builder: (context) => const Menu());
+    case HomePage.routeName:
+      return MaterialPageRoute(builder: (context) => const HomePage());
 
     default:
       return MaterialPageRoute(builder: (context) => const Menu());
--- lib/views/menu.dart
+++ lib/views/menu.dart
@@ -1,10 +1,120 @@
 import 'package:flutter/material.dart';
 
+import '../widget/custom_btn.dart';
+
 class Menu extends StatelessWidget {
   const Menu({super.key});
   static const String routeName = 'menu';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Icon(
+          Icons.menu,
+          size: 30,
+        ),
+        title: Text(
+          'Tokyo sushi',
+          style: TextStyle(color: Colors.grey[900], fontSize: 24),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          //promo banner
+          Container(
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              padding: EdgeInsets.all(18),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 138, 60, 55),
+                borderRadius: BorderRadius.circular(26),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(children: [
+                    Column(
+                      children: [
+                        Text(
+                          "Get 30% promo ",
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                        SizedBox(
+                          height: 12,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(212, 135, 81, 77),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              TextButton(
+                                onPressed: () {},
+                                child: const Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Text(
+                                    "redeem",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 20),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 8.0),
+                                child: const Icon(Icons.arrow_forward,
+                                    color: Colors.white, size: 24),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ]),
+                  Image.asset(
+                    "assets/images/sushi (4).png",
+                    height: 150,
+                    width: 150,
+                  )
+                ],
+              )),
+          SizedBox(
+            height: 25,
+          ),
+
+          //search abr
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: TextField(
+              decoration: InputDecoration(
+                fillColor: Colors.transparent,
+                filled: true,
+                prefixIcon: Icon(Icons.search),
+                hintText: 'Search',
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide:
+                      BorderSide(color: Color.fromARGB(255, 138, 60, 55)),
+                ),
+              ),
+            ),
+          ),
+
+          //categories
+          //category
+          SizedBox(
+            height: 20,
+          ),
+          //menu list
+
+          //popularfood
+        ],
+      ),
+    );
   }
 }
